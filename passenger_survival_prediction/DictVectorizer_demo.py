e47107979ba2776328d707eafc4b70effe9dcc03
JXY001A <JXY001A@aliyun.com>
2025-04-30 17:37:37 +0800

    feat：keep going

--- passenger_survival_prediction/DictVectorizer_demo.py
+++ passenger_survival_prediction/DictVectorizer_demo.py
@@ -4,10 +4,11 @@ from sklearn.feature_extraction import DictVectorizer
 data = [
     {'Age': 25, 'City': 'Beijing', 'Gender': 'Male'},
     {'Age': 30, 'City': 'Shanghai', 'Gender': 'Female'},
-    {'Age': 28, 'City': 'Beijing', 'Gender': 'Male'}
+    {'Age': 28, 'City': 'Beijing', 'Gender': 'Male'},
+    {'Age': 28, 'City': 'Nanjing', 'Gender': 'Male'}
 ]
 
-
+# 会为每一个枚举类型的特征开独立列
 dvec = DictVectorizer(sparse=False)
 X = dvec.fit_transform(data)
 
